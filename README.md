0c26b5cfd34283fb28f08cf7dbad0ca6f3928632
Rodrigues Filho <ednofco@gmail.com>
2016-02-11 12:43:48 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,4 @@
+## Prática 1 - Blink Led
+
+Prática com PIC18F2550. Exemplo que mostra como piscar um LED com um delay
+simples.
---
+++ blink-led.c
@@ -0,0 +1,14 @@
+/*
+ * File:   blink-led.c
+ * Author: edno
+ *
+ * Created on 11 de Fevereiro de 2016, 13:39
+ */
+
+
+#include <xc.h>
+#include <p18f2550.h>
+
+void main(void) {
+    return;
+}
